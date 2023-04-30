b6a4460eb020f10aeb8876387899aaf9836e0fec
Andrey Belokon <belokon1981andrey@gmail.com>
2023-04-30 23:51:41 +0300

    1

--- controllers/auth/googleAuth.js
+++ controllers/auth/googleAuth.js
@@ -2,12 +2,9 @@ const { User } = require('../../models/users');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-// const { SECRET_KEY, FRONTEND_URL } = process.env;
-const { SECRET_KEY } = process.env;
+const { SECRET_KEY, FRONTEND_URL } = process.env;
 
 const googleAuth = async (req, res) => {
-  const FRONTEND_URL_PERSONAL_PLAN =
-    'https://financial-planning-app-front.netlify.app/personal-plan';
   const { _id: id } = req.user;
   const payload = {
     id,
@@ -17,8 +14,7 @@ const googleAuth = async (req, res) => {
   await User.findByIdAndUpdate(id, { token });
 
   res.redirect(
-    // `${FRONTEND_URL}?email=${req.user.email}&password=${req.user.password}`
-    `${FRONTEND_URL_PERSONAL_PLAN}?email=${req.user.email}&password=${req.user.password}`
+    `${FRONTEND_URL}?email=${req.user.email}&password=${req.user.password}`
   );
 };
 
